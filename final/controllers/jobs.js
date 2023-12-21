620f8dcbc5cd1c92719c4e406c884294393e6edf
smajaas <smajaas@gmail.com>
2023-12-21 16:53:17 +0530

    pagination on the server

--- final/controllers/jobs.js
+++ final/controllers/jobs.js
@@ -22,8 +22,33 @@ const getAllJobs = async (req, res) => {
 
   let result = Job.find(queryObject);
 
+  // chain sort conditions
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
